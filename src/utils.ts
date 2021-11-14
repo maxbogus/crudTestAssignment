2cbdfeff0991fca0f810c43ab4c6d3509c320bec
Max Boguslavskiy <max.bogus@gmail.com>
2021-11-15 00:26:24 +0300

    handled errors

--- src/utils.ts
+++ src/utils.ts
@@ -5,22 +5,28 @@ import {Response, User} from './types';
 const getUser = async (page?: number): Promise<Response> => {
     const suffix = page !== undefined ? `?page=${page}` : '';
     const res = await fetch(`https://reqres.in/api/users${suffix}`);
+    if (res.error) {
+        throw Error(res.error);
+    }
     return await res.json();
 }
 
 export const fetchData = async () => {
     let result: User[];
 
-    const {page, total_pages, data, per_page} = await getUser();
-
-    result = data;
-    let index = page;
-    let limit = page;
-    while (limit <= total_pages) {
-        index += 1;
-        const {data} = await getUser(index);
-        result = [...result, ...data];
-        limit += per_page;
+    try {
+        const {page, total_pages, data, per_page} = await getUser();
+        result = data;
+        let index = page;
+        let limit = page;
+        while (limit <= total_pages) {
+            index += 1;
+            const {data} = await getUser(index);
+            result = [...result, ...data];
+            limit += per_page;
+        }
+    } catch (e) {
+        console.error(e)
     }
 
     return result;
