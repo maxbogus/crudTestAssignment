37639ee74d1046d2b5a9348fd5854320dbfbd7ea
Max Boguslavskiy <max.bogus@gmail.com>
2021-11-15 01:17:37 +0300

    commented fetch; wrote script for parser

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,24 @@
-import bodyParser from 'body-parser';
-import express from 'express';
-
+import {fetchData} from './api';
+import {parseData} from "./parser";
 import Employee from './models/users.model';
 import {EmployeeObject} from './models/types';
-import {fetchData} from './utils';
-
-// create express app
-const app = express();
-// Setup server port
-const port = process.env.PORT || 5000;
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}))
-// parse requests of content-type - application/json
-app.use(bodyParser.json())
-// define a root route
-app.get('/', (req, res) => {
-    res.send("Hello World");
-});
 
 // fetch data and store it in DB
-fetchData().then(result => {
-    // write down employees
-    result.forEach(user => {
-        const employee: EmployeeObject = Employee({first_name: user.first_name, avatar: user.avatar, last_name: user.last_name, email: user.email}) as EmployeeObject;
-        Employee.create(employee, (data) => console.log(data))
-    })
+// fetchData().then(result => {
+//     console.log('Start fetching data');
+//     // write down employees
+//     result.forEach(user => {
+//         const employee: EmployeeObject = Employee({first_name: user.first_name, avatar: user.avatar, last_name: user.last_name, email: user.email}) as EmployeeObject;
+//         Employee.create(employee, (data) => console.log(data))
+//     });
+//
+//     // show all employees
+//     Employee.findAll((test) => console.log(test));
+//
+//     // clear table
+//     Employee.deleteAll((test) => console.log(test));
+//     console.log('Stop fetching data');
+// });
 
-    // show all employees
-    Employee.findAll((test) => console.log(test));
-
-    // clear table
-    Employee.deleteAll((test) => console.log(test));
-});
+// init puppeteer
+parseData();
---
+++ src/parser/index.ts
@@ -0,0 +1,16 @@
+export const parseData = async () => {
+    console.log('Start puppeteer');
+
+    // init puppeteer
+    // start browser
+    // open page
+    // enter cred
+    // wait for page to load
+    // change dates
+    // wait for update . check for data
+    // change amount per page
+    // wait for change
+    // parse data
+
+    console.log('Stop puppeteer');
+};
