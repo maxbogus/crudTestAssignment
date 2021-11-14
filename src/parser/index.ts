5e1f0f6d676002ec78424d1462c5884f488e9fcd
Max Boguslavskiy <max.bogus@gmail.com>
2021-11-15 02:23:53 +0300

    experiments with close banner

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -18,30 +18,38 @@ export const parseData = async () => {
 
         // navigate to dates page
         await page.goto('https://develop.pub.afflu.net/list?type=dates');
+        await page.waitForTimeout(3000);
         await page.screenshot({path: 'screenshots/dates_page.png'});
 
-        // // select all dates
-        // await page.click('#datepicker', {delay:50});
-        // await page.type('input[name=daterangepicker_start]', '10/01/2021', {delay: 50});
-        // await page.type('input[name=daterangepicker_end]', '10/30/2021', {delay: 50});
-        // await page.click('button.applyBtn', {delay:50});
-        // await page.screenshot({path: 'screenshots/changed_dates.png'});
-        //
-        // // select all entries
-        // await page.click('button[title="5 entries"', {delay:50});
-        // await page.click('div.dropdown-menu.open ul li:last-child a', {delay:50});
-        // await page.click('button[title="5 entries"', {delay:50});
-        // await page.screenshot({path: 'screenshots/all_entries.png'});
-        //
-        // await page.screenshot({path: 'screenshots/datesPage.png'});
-        // // get data from table
-        // const result = await page.evaluate(() => {
-        //     const rows = document.querySelectorAll('table[data-url="dates"]');
-        //     // @ts-ignore
-        //     return Array.from(rows, row => row.innerText)
-        // });
-        //
-        // result.shift();
+        try {
+            await page.waitForTimeout(4000);
+            await page.click('#pushActionRefuse', {delay:50});
+        } catch (e) {
+            console.log('banner is already closed');
+        }
+
+        // select all dates
+        await page.click('#datepicker', {delay:50});
+        await page.type('input[name=daterangepicker_start]', '10/01/2021', {delay: 50});
+        await page.type('input[name=daterangepicker_end]', '10/30/2021', {delay: 50});
+        await page.click('button.applyBtn', {delay:50});
+        await page.screenshot({path: 'screenshots/changed_dates.png'});
+
+        // select all entries
+        await page.click('button[title="5 entries"', {delay:50});
+        await page.click('div.dropdown-menu.open ul li:last-child a', {delay:50});
+        await page.click('button[title="5 entries"', {delay:50});
+        await page.screenshot({path: 'screenshots/all_entries.png'});
+
+        await page.screenshot({path: 'screenshots/datesPage.png'});
+        // get data from table
+        const result = await page.evaluate(() => {
+            const rows = document.querySelectorAll('table[data-url="dates"]');
+            // @ts-ignore
+            return Array.from(rows, row => row.innerText)
+        });
+
+        result.shift();
         await browser.close();
     })();
 
