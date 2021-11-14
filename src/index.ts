4bc384344e6672b798f4a769a47c8ef47a908786
Max Boguslavskiy <max.bogus@gmail.com>
2021-11-15 00:16:52 +0300

    stored data locally

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import * as fetch from 'node-fetch';
+
+import {fetchData} from "./utils";
 
 // create express app
 const app = express();
 // Setup server port
 const port = process.env.PORT || 5000;
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({extended: true}))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 // define a root route
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
-
-interface Response {
-    page: number;
-    per_page: number;
-    total: number;
-    total_pages: number;
-    data: User[];
-}
-
-interface User {
-    id: number;
-    email: string;
-    first_name: string;
-    last_name: string;
-    avatar: string;
-}
-
-const getUser = async (page?: number): Promise<Response> => {
-    const suffix = page !== undefined ? `?page=${page}` : '';
-    const res = await fetch(`https://reqres.in/api/users}${suffix}`);
-    return await res.json();
-}
-
-const fetchData = async () => {
-    let result: User[];
-
-    const {page, total_pages, data, per_page} = await getUser();
-
-    result = data;
-    let index = page;
-    let limit = page;
-    while ( limit <= total_pages) {
-        index += 1;
-        const {data} = await getUser(index);
-        result = [...result, ...data];
-        limit += per_page;
-    }
-
-    return result;
-}
 
 fetchData().then(r => console.log(r));
 
---
+++ src/utils.ts
@@ -0,0 +1,27 @@
+import * as fetch from 'node-fetch';
+
+import {Response, User} from './types';
+
+const getUser = async (page?: number): Promise<Response> => {
+    const suffix = page !== undefined ? `?page=${page}` : '';
+    const res = await fetch(`https://reqres.in/api/users}${suffix}`);
+    return await res.json();
+}
+
+export const fetchData = async () => {
+    let result: User[];
+
+    const {page, total_pages, data, per_page} = await getUser();
+
+    result = data;
+    let index = page;
+    let limit = page;
+    while (limit <= total_pages) {
+        index += 1;
+        const {data} = await getUser(index);
+        result = [...result, ...data];
+        limit += per_page;
+    }
+
+    return result;
+}
